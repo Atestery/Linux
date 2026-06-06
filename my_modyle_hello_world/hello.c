6224f931f12c5ccf11380c424bd7a9955765bfd4
Andrey <ymnikg@gmail.com>
2026-06-06 17:16:14 +0300

    main add modyle hello.

---
+++ my_modyle_hello_world/hello.c
@@ -0,0 +1,23 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init hello_init(void)
+{
+	pr_info("Hello, World from the kernel!\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("Goodbye, World from the kernel!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Andrey");
+MODULE_DESCRIPTION("Hello World modyle");
+MODULE_VERSION("1.0");
