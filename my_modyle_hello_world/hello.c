f4c8edb21ca596049328b9a26df3a8443d945d35
Andrey <ymnikg@gmail.com>
2026-06-06 17:27:39 +0300

    main

--- my_modyle_hello_world/hello.c
+++ my_modyle_hello_world/hello.c
@@ -5,13 +5,13 @@
 
 static int __init hello_init(void)
 {
-	pr_info("Hello, World from the kernel!\n");
-	return 0;
+    pr_info("Hello, World from the kernel!\n");
+    return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	pr_info("Goodbye, World from the kernel!\n");
+    pr_info("Goodbye, World from the kernel!\n");
 }
 
 module_init(hello_init);
